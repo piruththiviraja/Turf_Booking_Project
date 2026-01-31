d4b29b1abf950b4fa906808b3e6b31cb05faa522
piruththiviraja <piruththiviraja@gmail.com>
2026-01-31 17:24:32 +0530

    vite.config.js updated for linux

--- frontend/turf_frontend/vite.config.js
+++ frontend/turf_frontend/vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Bind to all interfaces
-    port: 5173,      // Port to run the dev server
+  resolve: {
+    extensions: ['.js', '.jsx'], // explicitly resolve jsx
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // optional: use @/ for imports
+    },
+  },
+  build: {
+    outDir: 'dist',  // default Vite dist folder
   },
 })
